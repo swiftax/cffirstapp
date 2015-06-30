00ee0cb3b2118975bec252da850d13918a9e8ae5
Anthony Tropeano <tony@swiftax.com>
2015-06-30 12:47:24 -0400

    user changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-   user ||= User.new # guest user (not logged in)
-   can :manage, User, id: user.id 
-
+    user ||= User.new
+    can :manage, User, id: user.id
   end
 end
