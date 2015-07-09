f72a269734a6ebf91241381ef394294e95546065
Anthony Tropeano <tony@swiftax.com>
2015-07-09 17:10:49 -0400

    removed survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,6 +1,6 @@
 class SurveysController < ApplicationController
   before_action :set_survey, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!
   # GET /surveys
   # GET /surveys.json
   def index
