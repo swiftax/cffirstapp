25fec2c61d211415833285c25c7302841fd8a54f
Anthony Tropeano <tony@swiftax.com>
2015-07-09 10:57:03 -0400

    adjusted delete option

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -60,7 +60,7 @@ class ProductsController < ApplicationController
   # DELETE /products/1
   # DELETE /products/1.json
   def destroy
-    @product.destroy
+    @product.destroy 
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
       format.json { head :no_content }
