9cd97b2b2563d463339e856b10b8625fad40401d
Anthony Tropeano <tony@swiftax.com>
2015-07-15 11:07:38 -0400

    jquery additions

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,7 +1,36 @@
-$(document).on('ready page:load', function(){
-$('#tags').tagsInput({
-  autocomplete_url:'http://localhost:3000',
-  autocomplete:{selectFirst:true,width:'100px',autoFill:true
-	}
- });
-});
+$(document).ready ->
+  rows = $('.my-row')
+  i = 0
+  while i < rows.length
+    if i % 2 == 0
+      $(rows[i]).css 'background-color', 'pink'
+    ++i
+  return
+
+  $(document).ready ->
+  availableTags = [
+    'ActionScript'
+    'AppleScript'
+    'Asp'
+    'BASIC'
+    'C'
+    'C++'
+    'Clojure'
+    'COBOL'
+    'ColdFusion'
+    'Erlang'
+    'Fortran'
+    'Groovy'
+    'Haskell'
+    'Java'
+    'JavaScript'
+    'Lisp'
+    'Perl'
+    'PHP'
+    'Python'
+    'Ruby'
+    'Scala'
+    'Scheme'
+  ]
+  $('#tags').autocomplete source: availableTags
+  return
